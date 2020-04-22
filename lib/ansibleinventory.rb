8566db87e80a42e5f80d4aa05c63888b6023f605
Tomoyuki Sakurai <y@trombik.org>
2020-04-22 09:15:57 +0900

    bugfix: refactor the class
    * use Shellwords to escape inputs
    * cache command results
    * write tests

--- lib/ansibleinventory.rb
+++ lib/ansibleinventory.rb
@@ -1,11 +1,14 @@
 require "open3"
 require "yaml"
+require "shellwords"
 
 # Class to represent ansible inventory
 class AnsibleInventory
   attr_accessor :addnsible_inventory_path
   @config = nil
+  @config_list_hosts = nil
   @path = ""
+  @config_host = {}
 
   def self.VERSION
     "0.1.0"
@@ -26,13 +29,34 @@ class AnsibleInventory
   # Returns parsed inventory content
   def config
     return @config if @config
-    cmd = "#{ansible_inventory_path} --inventory '#{@path}' --yaml --list"
+    cmd = "#{ansible_inventory_path} --inventory #{Shellwords.escape(@path)} --yaml --list"
     o, e, s = run_command(cmd)
     unless s.success?
       warn e
       raise "failed to run `#{cmd}`: #{s}"
     end
     @config = YAML.safe_load(o)
+  end
+
+  def config_host(host)
+    return @config_host[host] if @config_host.key?(host)
+    cmd = "#{ansible_inventory_path} --inventory #{Shellwords.escape(@path)} --yaml --host #{Shellwords.escape(host)}"
+    o, e, s = run_command(cmd)
+    unless s.success?
+      warn e
+      raise "failed to run `#{cmd}`: #{s}"
+    end
+    @config[host] = YAML.safe_load(o)
+  end
+
+  def config_list_hosts(group)
+    return @config_list_hosts if @config_list_hosts
+    cmd = "#{ansible_path} --inventory #{Shellwords.escape(@path)} --list-hosts #{Shellwords.escape(group)}"
+    o, e, s = run_command(cmd)
+    unless s.success?
+      warn e
+      raise "failed to run `#{cmd}`: #{s}"
+    end
   end
 
   def run_command(cmd)
@@ -43,39 +67,39 @@ class AnsibleInventory
   # @param group [String] name of group
   # @return [Array] array of string of hosts in the group
   def all_hosts_in(group)
-    cmd = "#{ansible_path} --inventory '#{@path}' --list-hosts '#{group}'"
-    o, e, s = run_command(cmd)
-    unless s.success?
-      warn e
-      raise "failed to run `#{cmd}`: #{s}"
+    c = config
+    begin
+      children = c["all"]["children"]
+    rescue TypeError => e
+      warn c
+      raise RuntimeError, "BUG: unexpected inventory result from ansible-inventory"
     end
-    parse_list_hosts_output(o)
-  end
-
-  def parse_list_hosts_output(out)
-    result = out.split(/\n/)
-    result.shift # remove the first line
-    result.map { |i| i.gsub(/\s+/, "").tr("_", ".") }
+    hosts = []
+    return hosts unless children.key?(group)
+    if children[group].key?("hosts")
+      return children[group]["hosts"].keys
+    elsif children[group].key?("children")
+      hosts = []
+      children[group]["children"].keys.each do |child|
+        hosts += children[group]["children"][child]["hosts"].keys
+      end
+      return hosts
+    else
+      warn config
+      raise RuntimeError, "BUG: unexpected inventory result from ansible-inventory"
+    end
   end
 
   def host(host)
-    cmd = format(
-      "%s --inventory '%s' --yaml --host '%s'",
-      ansible_inventory_path, @path, host
-    )
-    o, e, s = run_command(cmd)
-    unless s.success?
-      warn e
-      raise "failed to run `#{cmd}`: #{s}"
-    end
-    YAML.safe_load(o)
+    return config_host(host)
   end
 
   def all_groups
-    groups = config["all"]["children"].keys
+    c = config
+    groups = c["all"]["children"].keys
     hidden_groups = []
     groups.each do |group|
-      hidden_groups += find_hidden_groups(config["all"]["children"][group])
+      hidden_groups += find_hidden_groups(c["all"]["children"][group])
     end
     (groups + hidden_groups).uniq
   end
--- spec/ansibleinventory_spec.rb
+++ spec/ansibleinventory_spec.rb
@@ -1,9 +1,93 @@
-RSpec.describe Ansibleinventory do
+RSpec.describe AnsibleInventory do
+  let(:inventory) { AnsibleInventory.new("foo/bar") }
+  let(:inventory_simple) { YAML.load_file("spec/fixtures/simple.yml") }
+  let(:inventory_simple_host) { YAML.load_file("spec/fixtures/simple_host.yml") }
+
   it "has a version number" do
-    expect(Ansibleinventory::VERSION).not_to be nil
+    expect(AnsibleInventory.VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".new" do
+    it "raises no error" do
+      expect{ AnsibleInventory.new("/foo/bar") }.not_to raise_error
+    end
+  end
+
+  describe "#ansible_inventory_path" do
+    it "is ansible-inventory" do
+      expect(inventory.ansible_inventory_path).to eq "ansible-inventory"
+    end
+  end
+
+  describe "#ansible_path" do
+    it "is ansible" do
+      expect(inventory.ansible_path).to eq "ansible"
+    end
+  end
+
+  describe "#all_groups" do
+    context "inventory is simple" do
+
+      it "returns array" do
+        allow(inventory).to receive(:config).and_return(inventory_simple)
+        expect(inventory.all_groups).to eq %w[mx ungrouped virtualbox virtualbox-credentials virtualbox-mx]
+      end
+    end
+  end
+
+  describe "#all_hosts_in" do
+    context "inventory is simple" do
+      before do
+        allow(inventory).to receive(:config).and_return(inventory_simple)
+      end
+      context "given a group that exists" do
+        it "returns mx1" do
+          expect(inventory.all_hosts_in("mx")).to eq ["mx1.trombik.org"]
+        end
+
+        it "returns mx1" do
+          expect(inventory.all_hosts_in("virtualbox")).to eq ["mx1.trombik.org"]
+        end
+
+        it "returns mx1" do
+          expect(inventory.all_hosts_in("virtualbox-mx")).to eq ["mx1.trombik.org"]
+        end
+      end
+
+      context "given a group that does not exist" do
+        it "returns empty array" do
+          expect(inventory.all_hosts_in("foo")).to eq []
+        end
+      end
+      context "given invalid arg" do
+        it "returns empty array" do
+          expect(inventory.all_hosts_in(nil)).to eq []
+          expect(inventory.all_hosts_in(false)).to eq []
+          expect(inventory.all_hosts_in([])).to eq []
+          expect(inventory.all_hosts_in({})).to eq []
+        end
+      end
+    end
+    context "inventory is invalid" do
+      before do
+        allow(inventory).to receive(:config).and_return([])
+      end
+
+      it "raise exception" do
+        expect{ inventory.all_hosts_in("foo") }.to raise_error(RuntimeError)
+      end
+    end
+  end
+
+  describe "#host" do
+    context "inventory is simple_host" do
+      before do
+        allow(inventory).to receive(:config_host).with("mx1.trombik.org").and_return(inventory_simple_host)
+      end
+
+      it "returns mx1.trombik.org" do
+        expect(inventory.host("mx1.trombik.org")).to include("ansible_host" => "172.16.100.100", "vagrant_priority" => 10)
+      end
+    end
   end
 end
